f5aee6edcfa876dfef4570cd0b7164058a7f2ebe
Andrebravo23 <hbravo798@gmail.com>
2022-09-18 21:29:54 -0500

    responsive navbar ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
-const navbar = document.getElementById('navbar');
+const body = document.querySelector('body');
+const header = document.getElementById('header');
 let prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
-const currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    navbar.style.top = 0;
-  } else {
-    navbar.style.top = '-' + navbar.offsetHeight + 'px';
-  }
-  prevScrollpos = currentScrollPos;
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        header.style.top = 0;
+    } else {
+        header.style.top = '-' + header.offsetHeight + 'px';
+    }
+    prevScrollpos = currentScrollPos;
 }
+
+const navbar = document.getElementById('navbar');
+const toggleNav = document.querySelectorAll('.btn-nav');
+
+toggleNav.forEach(btn => {
+    btn.addEventListener('click', () => {
+        navbar.classList.toggle('show');
+        body.classList.toggle('noscroll');
+    });
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('show');
+        body.classList.remove('noscroll');
+    });
+});
